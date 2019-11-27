acaa8aa499e3a43f04fd96fa4f102b0dc0d81f65
teleti50 <andreea.teletin@tss-yonder.com>
2019-11-27 15:04:38 +0200

    add new project

---
+++ src/main/java/guru/springframework/gurumsscbrewery/GuruMsscBreweryApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.gurumsscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruMsscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruMsscBreweryApplication.class, args);
+	}
+
+}
